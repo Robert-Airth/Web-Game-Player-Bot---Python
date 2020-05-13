9820a02662c156f23fb39f836b2d2ff40f5994f8
rja45 <54596406+rja45@users.noreply.github.com>
2020-05-13 12:30:00 -0700

    Delete userinfo.py

--- userinfo.py
+++
@@ -1,6 +0,0 @@
-userlist1 = {"rob": ["robadrifter", "Kikiman1!"],
-         "hayley": ["haykey1", "Motivation420"],
-         "paulette": ["webkinzmom03", "Hayley03"]}
-
-userlist2 = {"rob": ["robadrifter", "Kikiman1!"],
-         "hayley": ["haykey1", "Motivation420"]}
