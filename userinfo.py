17b6622aea995894e68d505554a2bd1bf3545a38
rja45 <54596406+rja45@users.noreply.github.com>
2020-05-13 00:13:42 -0700

    added userinfo.py to gitignore
    added userinfo.py to gitignore

--- userinfo.py
+++ userinfo.py
@@ -1,2 +1,6 @@
-username = "robadrifter"
-password = "Kikiman1!"
+userlist1 = {"rob": ["robadrifter", "Kikiman1!"],
+         "hayley": ["haykey1", "Motivation420"],
+         "paulette": ["webkinzmom03", "Hayley03"]}
+
+userlist2 = {"rob": ["robadrifter", "Kikiman1!"],
+         "hayley": ["haykey1", "Motivation420"]}
